39d10014a38e33ecab3a9194853e0389376d2146
Kadir Kuruca <kadirkuruca572@gmail.com>
2024-02-01 01:11:51 +0300

    Create animations and set for movements

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,9 +5,8 @@ const FRICTION = 1000.0
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var animated_sprite_2d = $AnimatedSprite2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -18,7 +17,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)	
-
+	update_animations(input_axis)
 	move_and_slide()
 
 func apply_gravity(delta):
@@ -32,3 +31,13 @@ func handle_jump():
 	else:
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
 			velocity.y = JUMP_VELOCITY / 2
+			
+func update_animations(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.flip_h = (input_axis < 0)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+		
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
