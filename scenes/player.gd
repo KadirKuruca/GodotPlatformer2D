66f2d804d0df150f46a5ee10ce2c62d373902718
Kadir Kuruca <kadirkuruca572@gmail.com>
2024-02-01 14:46:27 +0300

    Implement air_acceleration to player
    refactor code

--- scenes/player.gd
+++ scenes/player.gd
@@ -10,11 +10,8 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
-	if input_axis == 0 and is_on_floor():
-		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
-		
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
@@ -22,6 +19,17 @@ func _physics_process(delta):
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
+		
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+		else:
+			velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_accelaration * delta)				
+
+func apply_friction(input_axis, delta):
+	if input_axis == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, movement_data.friction * delta)
 
 func apply_gravity(delta):
 	if not is_on_floor():
