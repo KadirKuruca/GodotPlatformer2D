ab98d07c7a89fe8e54e1503e3fefd6f498aa4647
Kadir Kuruca <kadirkuruca572@gmail.com>
2024-02-04 21:33:42 +0300

    Focus on start game button

--- scenes/start_menu.gd
+++ scenes/start_menu.gd
@@ -1,5 +1,10 @@
 extends CenterContainer
 
+@onready var start_game_button = %StartGameButton
+
+func _ready():
+	start_game_button.grab_focus()
+
 func _on_start_game_button_pressed():
 	await LevelTransition.fade_to_black()
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
